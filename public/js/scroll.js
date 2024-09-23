46c5fe8ce69be2a358c74ee1da4206779b987f39
Dayeong <ydiosa98@gmail.com>
2024-09-24 08:43:31 +0900

    feat:  Section 구분 및 스크롤 이벤트 작성

---
+++ public/js/scroll.js
@@ -0,0 +1,37 @@
+/* 새로고침 시 스크롤 위치 복원 안함 */
+history.scrollRestoration = "manual";
+
+window.onload = () => {
+    let page = 0;
+    let animated = false;
+
+    const main = document.querySelector('main');
+        
+    main.addEventListener('wheel', e => {
+      e.preventDefault();
+      
+        if(!animated) {
+            animated = true;
+            setTimeout(() => {
+                animated = false;
+            }, 500);
+        
+            if(e.deltaY < 0) {
+                if(page === 0)
+                    return;
+                page--;
+            }
+        
+            if(e.deltaY > 0) {
+                if(page === 4)
+                    return;
+                page++;
+            }
+        
+            window.scroll({
+                top: window.innerHeight * page, 
+                behavior: "smooth",
+            });
+        }
+    });
+}
