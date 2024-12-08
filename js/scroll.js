399e0eb180829edddfaca825781c96432567d5ad
Dayeong <ydiosa98@gmail.com>
2024-12-08 23:20:23 +0900

    feat: 스크롤 시 실시간으로 현재 Page를 저장하는 기능 구현(Intersection Observer API 적용)

--- js/scroll.js
+++ js/scroll.js
@@ -6,17 +6,21 @@ let maxPage = 4;
 let animated = false;
 let startY = null;
 
+let observer;   // Intersection Observer 저장 변수
+
 let minHeight = 680;    // 섹션의 최소 높이
 let isOnePageScroll = true;
 
 window.addEventListener('load', () => {
     const wrapper = document.getElementById('wrapper');
     const nav = document.querySelectorAll('nav ul li');
-
     const overflowY = getComputedStyle(wrapper).overflowY;
 
     // 모바일에서 주소 표시줄 유무 및 키보드 유무에 따라 100vh가 정확한 높이를 반영할 수 있도록 동적 처리
     adjustHeight();
+
+    // Intersection Observer 생성 및 시작
+    createObserver();
 
     // 원 페이지 스크롤이 적용되는 환경인지 확인
     isOnePageScroll = (overflowY !== 'scroll');
@@ -54,7 +58,7 @@ window.addEventListener('load', () => {
         }
     }, {passive: false});
 
-    // touchstart 시 터치 슬라이드 방향 확인을 위한 터치 시작 지점 Y값 저장(모바일)
+    // 터치 슬라이드 방향 확인을 위한 터치 시작(touchstart) 지점 Y값 저장(모바일)
     window.addEventListener('touchstart', e => {
         startY = e.touches[0].clientY; 
     }, {passive: false});
@@ -97,18 +101,7 @@ window.addEventListener('load', () => {
         }
     }, {passive: false});
 
-    wrapper.addEventListener('scroll', e => {
-        // 뷰포트 높이 변경 시 이전 스크롤 위치에 해당되는 page 저장    // 일반 스크롤 시에만 해당
-        let sectionHeight = Math.max(window.innerHeight, minHeight);
-        let curr = Math.round(wrapper.scrollTop / sectionHeight);
-
-        if(curr === page) return;
-        if(curr < 0 || curr > maxPage - 1) return;  // 마지막 페이지인 footer는 section 높이보다 훨씬 작기 때문에 제외
-
-        page = curr;
-    })
-
-    // nav 메뉴 클릭 이벤트
+    // 메뉴 클릭 시 해당 섹션으로 자동 스크롤
     for(let i = 0; i < nav.length; i++) {
         let el = nav[i];
 
@@ -122,8 +115,11 @@ window.addEventListener('load', () => {
 let timer = null;
 let initialHeight = window.innerHeight;
 
-// 윈도우 높이 변경 시 해당 섹션으로 자동 스크롤
+// 뷰포트 크기 변경 시 원래 위치하던 섹션으로 자동 스크롤
 window.addEventListener('resize', () => { 
+    // resize 시 모든 observer 종료
+    if (observer) observer.disconnect();
+
     // 모바일에서 주소 표시줄 유무 및 키보드 유무에 따라 100vh가 정확한 높이를 반영할 수 있도록 동적 처리
     adjustHeight();
 
@@ -135,8 +131,34 @@ window.addEventListener('resize', () => {
         // 원 페이지 스크롤이 적용되는 환경인지 확인
         isOnePageScroll = (overflowY !== 'scroll');
 
-        // 현재 페이지에 맞게 스크롤
+        // 현재 페이지에 맞게 자동 스크롤
         scrollEvent(wrapper);
+
+        let previousScrollTop = null;   // 이전 스크롤 위치
+        let num = 60;   // 최대 반복 횟수
+
+        /**
+         * 스크롤 이벤트가 종료되었는지 확인하는 함수
+         */
+        const waitForScrollAnimation = () => {
+            if(num-- <= 0) return;
+
+            // 현재 스크롤 위치 저장
+            const currentScrollTop = wrapper.scrollTop;
+
+            // 스크롤 위치가 더 이상 변경되지 않으면 createObserver 함수 호출
+            if (currentScrollTop === previousScrollTop) {
+                // Intersection Observer 재 생성 및 시작
+                createObserver();
+                return;
+            }
+            
+            previousScrollTop = currentScrollTop;
+            requestAnimationFrame(waitForScrollAnimation);  // waitForScrollAnimation 반복
+        }
+
+        // 스크롤 이벤트가 끝날 때까지 대기
+        requestAnimationFrame(waitForScrollAnimation);
 	}, 300);
 });
 
@@ -149,8 +171,8 @@ const adjustHeight = () => {
 }
 
 /**
- * 윈도우 스크롤 이벤트 함수
- * @param {*} el 스크롤 Element 
+ * 해당 요소의 스크롤 이벤트 함수
+ * @param {*} el 스크롤을 적용할 요소(Element)
  */
 const scrollEvent = (el) => {
     let targetScrollTop = page * Math.max(window.innerHeight, minHeight);
@@ -159,4 +181,27 @@ const scrollEvent = (el) => {
         top: targetScrollTop, 
         behavior: "smooth",
     });
+}
+
+/**
+ * 관찰 중인 섹션 인덱스 확인 후 page 저장 함수 (Intersection Observer API 사용) 
+ */
+const createObserver = () => {
+    const sections = document.querySelectorAll('main > section');
+
+    // Intersection Observer 생성
+    observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // 현재 위치한 섹션의 인덱스 추출 및 page 변수에 저장
+                const index = Array.from(sections).indexOf(entry.target);
+                page = index;
+            }
+        });
+    }, {
+        threshold: 0.5 // 섹션이 50% 이상 보일 때
+    });
+    
+    // Observer 시작
+    sections.forEach((section) => observer.observe(section));
 }
