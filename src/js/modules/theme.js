b58cd72ba669630139349ba1029f487f8bd9604d
Dayeong <ydiosa98@gmail.com>
2025-01-09 16:07:10 +0900

    feat: 설정한 테마 유지하는 기능 구현(Local Storage에 테마 정보 저장)

--- src/js/modules/theme.js
+++ src/js/modules/theme.js
@@ -6,14 +6,33 @@ const changeTheme = () => {
 
     document.body.classList.toggle("dark_mode");
 
-    if(isDarkMode) 
+    if(isDarkMode) {
         theme_toggle.classList.replace("fa-sun", "fa-moon");
-    else
+        localStorage.setItem("theme", "light");
+    }
+    else {
         theme_toggle.classList.replace("fa-moon", "fa-sun");
+        localStorage.setItem("theme", "dark");
+    }
+}
+
+const setTheme = () => {
+    const theme = localStorage.getItem("theme");
+    const theme_toggle = themeElements.theme_toggle;
+    
+    if(theme === "dark") {
+        document.body.classList.add("dark_mode");
+        theme_toggle.classList.replace("fa-moon", "fa-sun");
+    }
+
+    document.body.style.visibility = "visible";
 }
 
 const initializeTheme = () => {
     const theme_toggle = themeElements.theme_toggle;
+
+    setTheme();
+    window.addEventListener('storage', setTheme, false);
 
     theme_toggle.addEventListener('click', changeTheme);
 }
